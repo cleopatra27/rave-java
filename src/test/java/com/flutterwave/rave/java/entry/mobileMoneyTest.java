f0bc59f4800e0c2bc9858c03e6921264cd3640b2
cleopatra douglas <cleopatradouglas@live.com>
2019-08-05 09:42:36 +0100

    Update mobileMoneyTest.java

--- src/test/java/com/flutterwave/rave/java/entry/mobileMoneyTest.java
+++ src/test/java/com/flutterwave/rave/java/entry/mobileMoneyTest.java
@@ -26,30 +26,6 @@ public class mobileMoneyTest {
     @Test
     public void testDomobilemoney() throws Exception {
 
-        mobileMoney mobileMoney = new mobileMoney();
-        mobilemoneyPayload mobilemoneyPayload = new mobilemoneyPayload();
-        mobilemoneyPayload.setPBFPubKey(raveConfig.PUBLIC_KEY);
-        mobilemoneyPayload.setCurrency("ZMW");
-        mobilemoneyPayload.setAmount("50");
-        mobilemoneyPayload.setPhonenumber("054709929220");
-        mobilemoneyPayload.setEmail("user@example.com");
-        mobilemoneyPayload.setFirstname("temi");
-        mobilemoneyPayload.setLastname("desola");
-        mobilemoneyPayload.setNetwork("MTN");
-        mobilemoneyPayload.setCountry("NG");
-        mobilemoneyPayload.setDevice_fingerprint("69e6b7f0b72037aa8428b70fbe03986c");
-        mobilemoneyPayload.setPayment_type("mobilemoneyzambia");
-        mobilemoneyPayload.setEncryption_key("FLWSECK_TEST6d4fddc11a53");
-        mobilemoneyPayload.setPublic_key(raveConfig.PUBLIC_KEY);
-        mobilemoneyPayload.setTest("1");
-//set test to 1 if sandbox and 0 if live
-
-        String expected = "success";
-        String response = mobileMoney.domobilemoney(mobilemoneyPayload);
-        JSONObject RES = new JSONObject(response);
-        String status = RES.optString("status");
-
-        assertEquals(expected, status);
     }
 
 }
